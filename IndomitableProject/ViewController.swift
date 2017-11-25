d7056b73886b63221d1eaf94a2056ffddedf3a24
Yudi <yudiajalah2@gmail.com>
2017-11-25 17:11:52 +0700

    ini AddNewTaskMenu
    mending di satuin menurut w ketimbang tiap input beda screen

--- IndomitableProject/ViewController.swift
+++ IndomitableProject/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    var CategegoryCollectionData: [String] = ["Satu", "Dua", "Tiga", "Empat"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        CategoryCollection.dataSource = self
+        CategoryCollection.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var CategoryCollection: UIPickerView!
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
+    @IBAction func categoryNext(_ sender: Any) {
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CategegoryCollectionData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CategegoryCollectionData[row]
+    }
 
 }
 
