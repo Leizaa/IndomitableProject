ee75ef815f100d2d5e33b101f0042faa6bdce576
metta <mettasaridewi.w@gmail.com>
2017-11-28 09:19:37 +0700

    Commit final changes and added subfolders

---
+++ IndomitableProject/PlanTableController.swift
@@ -0,0 +1,13 @@
+//
+//  PlanTableController.swift
+//  IndomitableProject
+//
+//  Created by Metta Saridewi Wahab on 28/11/17.
+//  Copyright © 2017 RagingWind. All rights reserved.
+//
+
+import UIKit
+
+class PlanTableController: UIViewController {
+    
+}
