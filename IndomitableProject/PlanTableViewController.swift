ad9e147ef655c89ffaa5b5f7f105136009ba3540
metta <mettasaridewi.w@gmail.com>
2017-11-29 15:15:23 +0700

    Fixed Plan Storyboard
    requires core data to function

--- IndomitableProject/PlanTableViewController.swift
+++ IndomitableProject/PlanTableViewController.swift
@@ -8,36 +8,50 @@
 
 import UIKit
 
-class PlanTableViewController: UIViewController {
+class PlanTableViewController: UITableViewController {
     @IBOutlet weak var planTableView: UITableView!
-    var plans: [Plan] = project.schedule.plans //get plans from project
+    var plans: [Plan]! //get plans from project
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        planTableView.dataSource = self
+        plans = manageProject()
+        
+        print(plans.count)
     }
     
-}
-
-extension PlanTableViewController: UITableViewDataSource{
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("b")
+        return plans.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        print("c")
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = planTableView.dequeueReusableCell(withIdentifier: "planCell", for: indexPath) as! PlanTableViewCell
+        print("a")
+        // Configure the cell...
         let cellData = plans[indexPath.row]
         
         cell.name.text = cellData.name
         cell.days.text = "\(cellData.duration)d"
+        
         return cell
     }
     
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func manageProject() -> [Plan] {
+        //initialize project, tasks.
+        var plan: Plan = Plan(name: "Planning")
+        //var plan2: Plan = Plan(name: "Sprinting")
+        plan.addTask(task: Task(name: "Design", memberCount: 1, time: Time(duration: 10, unit: .days)))
+        plan.addTask(task: Task(name: "Code", memberCount: 1, time: Time(duration: 5, unit: .days)))
+        plan.addTask(task: Task(name: "Testing", memberCount: 1, time: Time(duration: 7, unit: .days)))
         
-        return plans.count
+        project.schedule.plans.append(plan)
+        return project.schedule.plans
     }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-
 }
+
+
