c50fe46bfaf4dd0daaf435fa9953053c81a7814c
metta <mettasaridewi.w@gmail.com>
2017-11-29 12:59:59 +0700

    added data source but no luck updating Plan table view

--- IndomitableProject/PlanTableViewController.swift
+++ IndomitableProject/PlanTableViewController.swift
@@ -14,11 +14,24 @@ class PlanTableViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        planTableView.dataSource = self
     }
+    
 }
 
 extension PlanTableViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = planTableView.dequeueReusableCell(withIdentifier: "planCell", for: indexPath) as! PlanTableViewCell
+        let cellData = plans[indexPath.row]
+        
+        cell.name.text = cellData.name
+        cell.days.text = "\(cellData.duration)d"
+        return cell
+    }
+    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return plans.count
     }
     
@@ -26,22 +39,5 @@ extension PlanTableViewController: UITableViewDataSource{
         return 1
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = planTableView.dequeueReusableCell(withIdentifier: "planCell", for: indexPath) as! PlanTableViewCell
-        let cellData = plans[indexPath.row]
-        
-        cell.name.text = cellData.name
-        cell.days.text = "\(cellData.duration)"
-//        cell.title.text = cellData.movieTitle
-//        cell.genre.text = cellData.movieGenre
-//        cell.desc.text = cellData.movieDescription
-//        if let image = UIImage(named: cellData.movieImage) {
-//            cell.mainImage?.image = image
-//        }
-        //        if let image = UIImage(named: "a4") {
-        //            cell.imageView?.image = image
-        //        }
-        
-        return cell
-    }
+
 }
